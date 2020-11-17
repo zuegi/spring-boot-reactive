46f8eb881709dea8bdacf8a3f7ef1a8063dbe425
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-11-17 16:13:54 +0100

    zuegi initial commit

---
+++ src/test/java/ch/wesr/dojo/springbootreactive/SpringBootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wesr.dojo.springbootreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
